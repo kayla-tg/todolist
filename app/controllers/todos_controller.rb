e3039c8e7d13f87e56e7fded674e37a1d726602d
Kayla George <george@ronclarkacademy.com>
2025-04-23 14:01:09 -0400

    adding todo_params, toggle_complete method (for checkboxes), and update method (for editing)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,16 +4,32 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.create(description: params[:todo][:description])
-    if @todo.valid?
-      redirect_to todos_path 
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path if @todo.valid?
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to todos_path
     end
+  end
+
+  def toggle_complete
+    @todo = Todo.find(params[:id])
+    @todo.update(completed: !@todo.completed)
+    redirect_to todos_path
   end
 
   def destroy
     @todo = Todo.find(params[:id])
-    if @todo.destroy
-      redirect_to todos_path
-    end
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
   end
 end
