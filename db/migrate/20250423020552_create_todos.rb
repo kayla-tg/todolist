67736c367234f6e5b15dc58398bc969009071a73
Kayla George <george@ronclarkacademy.com>
2025-04-23 06:04:27 -0400

    updating migration for checkbox boolean

--- db/migrate/20250423020552_create_todos.rb
+++ db/migrate/20250423020552_create_todos.rb
@@ -2,6 +2,7 @@ class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
       t.string :description
+      t.boolean :completed, default: false
 
       t.timestamps
     end
