1b2a7de569c02b1c9cbe52265883d9fb6c8f48eb
Kayla George <george@ronclarkacademy.com>
2025-04-22 22:44:55 -0400

    added create method for todos controller - creating new todo object with user input description on submit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,11 @@
 class TodosController < ApplicationController
   def index
-    
+  end
+
+  def create
+    @todo = Todo.create(description: params[:todo][:description])
+    if @todo.valid?
+      redirect_to todos_path 
+    end
   end
 end
